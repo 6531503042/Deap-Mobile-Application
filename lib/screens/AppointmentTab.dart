a44128b803562a948120fb6902cf9a3573cd9659
Klavivach <66194783+MABiuS1@users.noreply.github.com>
2024-04-28 22:37:39 +0700

    fix appointmentTab and delete book_apmt

--- lib/screens/AppointmentTab.dart
+++ lib/screens/AppointmentTab.dart
@@ -36,62 +36,66 @@ class _AppointState extends State<AppointmentTab> {
       appBar: AppBar(
         title: Text('Booking Calendar'),
       ),
-      body: Column(
-        children: [
-          // Calendar widget to select date
-          CalendarDatePicker(
-            initialDate: selectedDate,
-            onDateChanged: (DateTime newDate) {
-              setState(() {
-                selectedDate = newDate;
-              });
-            },
-          ),
-          // Text to display selected date
-          Text(
-            'Selected Date: ${selectedDate.toIso8601String()}',
-            style: TextStyle(fontSize: 16.0),
-          ),
-          SizedBox(height: 10.0),
-          // List of available times
-          Wrap(
-            spacing: 10.0,
-            children: availableTimes.map((time) {
-              return ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    selectedTime = time;
-                  });
-                },
-                child: Text(time),
-                style: ElevatedButton.styleFrom(
-                  primary: selectedTime == time
-                      ? Color.fromARGB(255, 60, 242, 203)
-                      : Colors.grey,
-                ),
-              );
-            }).toList(),
-          ),
-          SizedBox(height: 10.0),
-          // Button to confirm booking (replace with your booking logic)
-          ElevatedButton(
-            onPressed: () {
-              if (selectedTime.isNotEmpty) {
-                // Handle booking logic here
-                print(
-                    'Booking confirmed for ${selectedDate.toIso8601String()} at $selectedTime');
-              } else {
-                // Show a message if no time is selected
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('Please select a time slot'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Calendar widget to select date
+            CalendarDatePicker(
+              firstDate: DateTime.now(),
+              initialDate: selectedDate,
+              onDateChanged: (DateTime newDate) {
+                setState(() {
+                  selectedDate = newDate;
+                });
+              },
+              lastDate: DateTime.now(),
+            ),
+            // Text to display selected date
+            Text(
+              'Selected Date: ${selectedDate.toIso8601String()}',
+              style: TextStyle(fontSize: 16.0),
+            ),
+            SizedBox(height: 10.0),
+            // List of available times
+            Wrap(
+              spacing: 10.0,
+              children: availableTimes.map((time) {
+                return ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedTime = time;
+                    });
+                  },
+                  child: Text(time),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: selectedTime == time
+                        ? Color.fromARGB(255, 60, 242, 203)
+                        : Colors.grey,
                   ),
                 );
-              }
-            },
-            child: Text('Book Appointment'),
-          ),
-        ],
+              }).toList(),
+            ),
+            SizedBox(height: 10.0),
+            // Button to confirm booking (replace with your booking logic)
+            ElevatedButton(
+              onPressed: () {
+                if (selectedTime.isNotEmpty) {
+                  // Handle booking logic here
+                  print(
+                      'Booking confirmed for ${selectedDate.toIso8601String()} at $selectedTime');
+                } else {
+                  // Show a message if no time is selected
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Please select a time slot'),
+                    ),
+                  );
+                }
+              },
+              child: Text('Book Appointment'),
+            ),
+          ],
+        ),
       ),
     );
   }
