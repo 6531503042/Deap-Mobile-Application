94de99386a5711c3e420891bb8a2163c3641988c
Softie_tie <6531503005@lamduan.mfu.ac.th>
2024-05-01 00:31:12 +0700

    Update Apmt_confirmation.dart

--- lib/screens/Apmt_confirmation.dart
+++ lib/screens/Apmt_confirmation.dart
@@ -1,107 +1,155 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class AppointmentConfirmationPage extends StatelessWidget {
-  final DateTime selectedDate;
-  final String selectedTime;
-  final String selectedDoctor; // Add doctor name variable
+class AppointmentTab extends StatefulWidget {
+  const AppointmentTab({Key? key}) : super(key: key);
 
-  const AppointmentConfirmationPage({
-    Key? key,
-    required this.selectedDate,
-    required this.selectedTime,
-    required this.selectedDoctor,
-  }) : super(key: key);
+  @override
+  _AppointmentTabState createState() => _AppointmentTabState();
+}
+
+class _AppointmentTabState extends State<AppointmentTab> {
+  DateTime selectedDate = DateTime.now();
+  String selectedTime = "";
+
+  final List<String> availableTimes = [
+    "09:00 AM", "09:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM",
+    "12:00 PM", "12:30 PM", "01:00 PM", "01:30 PM", "02:00 PM", "02:30 PM",
+    "03:00 PM", "03:30 PM", "04:00 PM", "04:30 PM", "05:00 PM", "05:30 PM",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Appointment Confirmation'),
+        title: Text('Booking Appointments'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            // Display selected date
-            Text(
-              'Selected Date: ${DateFormat.yMd().format(selectedDate)}',
-              style: const TextStyle(fontSize: 18),
+            CalendarDatePicker(
+              firstDate: DateTime.now(),
+              initialDate: selectedDate,
+              onDateChanged: (DateTime newDate) {
+                setState(() {
+                  selectedDate = newDate;
+                });
+              },
+              lastDate: DateTime.now().add(Duration(days: 30)),
             ),
-            const SizedBox(height: 16),
-            // Display selected time
-            Text(
-              'Selected Time: $selectedTime',
-              style: const TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 16),
-            // Display selected doctor
-            Text(
-              'Selected Doctor: $selectedDoctor',
-              style: const TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 16),
-            const Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Write your problem condition',
-                  border: OutlineInputBorder(),
-                ),
-                maxLines: null,
+            SizedBox(height: 10.0),
+            Container(
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 103, 223, 213),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.calendar_today),
+                  SizedBox(width: 10.0),
+                  Text(
+                    '${DateFormat.yMMMMd().format(selectedDate)}',
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 16),
+            SizedBox(height: 10.0),
+            Wrap(
+              spacing: 10.0,
+              children: availableTimes.map((time) {
+                return ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      selectedTime = time;
+                    });
+                  },
+                  child: Text(time),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: selectedTime == time
+                        ? Color.fromARGB(255, 60, 242, 203)
+                        : Color.fromARGB(255, 255, 255, 255),
+                  ),
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 10.0),
             ElevatedButton(
               onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: const Text('Congratulations!'),
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.center, // Center content horizontally
-                        children: [
-                          // Add image (logo) here
-                          Image.asset(
-                            'assets/verify.png',
-                            height: 100, // Adjust height as needed
-                          ),
-                          const SizedBox(height: 16),
-                          Text('Your appointment with $selectedDoctor is confirmed for'),
-                          Text(
-                            '${DateFormat.MMMM().format(selectedDate)} ${selectedDate.day}, ${selectedDate.year}, at $selectedTime .',
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                      actions: [
-                        Center( // Center the button
-                          child: TextButton(
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                              decoration: BoxDecoration(
-                                color: const Color.fromARGB(255, 71, 202, 167),
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              child: const Text(
-                                'Done',
-                                style: TextStyle(color: Colors.white),
+                if (selectedTime.isNotEmpty) {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/verify.png',
+                              height: 100,
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              'Congratulations!',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(height: 16),
+                            Text('Your appointment is confirmed for'),
+                            Text(
+                              '${DateFormat.MMMM().format(selectedDate)} ${selectedDate.day}, ${selectedDate.year}, at $selectedTime .',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        actions: [
+                          Center( // Center the button
+                            child: TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Container(
+                                padding: EdgeInsets.symmetric(vertical: 10),
+                                width: 240, // Set the width of the container
+                                decoration: BoxDecoration(
+                                  color: Color.fromARGB(255, 71, 202, 167),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Text(
+                                  'Done',
+                                  style: TextStyle(color: Colors.white),
+                                  textAlign: TextAlign.center,
+                                ),
                               ),
                             ),
-                            onPressed: () {
-                              // TODO: Implement appointment confirmation logic.
-                              Navigator.of(context).pop();
-                            },
                           ),
-                        ),
-                      ],
-                    );
-                  },
-                );
+                        ],
+                      );
+                    },
+                  );
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Please select a time slot'),
+                    ),
+                  );
+                }
               },
-              child: const Text('Confirm Appointment'),
+              child: Text('Book Appointment'),
             ),
           ],
         ),
